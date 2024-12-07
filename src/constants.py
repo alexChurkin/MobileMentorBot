6216ffe0d11abce222ebcf868e835910324a46c3
Korobeynikova Alice <84507809+wellcome2AD@users.noreply.github.com>
2024-12-07 18:13:18 +0300

    create user roles and different commands set for each (#1)
    add login
    add teacher's commands for answering questions
    add pupil's commands for observing and deleting his questions
    add empty contexts for users
    pupil commands are ready
    add commands for showing module and topic for teacher
    add user context for commands
    add commands for adding new module and topic
    add change module and topic commands for teacher
    add delete module and topic commands for teacher
    all commands are fixed
    delete filling db from json file
    
    Co-authored-by: a.korobeynikova <a.korobeynikova@yadro.com>

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,9 @@
 import os
 
 
-MOBMENTOR_BOT_TOKEN = "7581877647:AAEjqoONxSUx9Gg46IGU25wMqO1kCKkH_24"
-MOBMENTOR_DB_FILE_NAME = "mobmentor.db"
-MOBMENTOR_DB_SCRIPT_PATH = os.path.join("src", "sql", "create_tables.sql")
+MOBMENTOR_BOT_TOKEN             = "7581877647:AAEjqoONxSUx9Gg46IGU25wMqO1kCKkH_24"
+MOBMENTOR_DB_FILE_NAME          = "mobmentor.db"
+MOBMENTOR_DB_SCRIPT_PATH        = os.path.join("src", "sql", "create_tables.sql")
+PUPIL_ROLE_TEXT                 = "pupil"
+TEACHER_ROLE_TEXT               = "teacher"
+DEFAULT_PSSWD_FOR_TEACHER_LOGIN = "secret_password"
--- src/sql/create_tables.sql
+++ src/sql/create_tables.sql
@@ -1,50 +1,48 @@
-DROP TABLE IF EXISTS Modules;
-DROP TABLE IF EXISTS Topics;
-DROP TABLE IF EXISTS AnsweredQuestions;
-DROP TABLE IF EXISTS OpenQuestions;
-DROP VIEW IF EXISTS TopicsView;
-
-CREATE TABLE Modules (
-        module_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        module_name TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS Modules (
+        module_id    INTEGER PRIMARY KEY AUTOINCREMENT,
+        module_name  TEXT NOT NULL,
         module_descr TEXT
 );
 
-CREATE TABLE Topics (
-        module_id INTEGER NOT NULL,
-        topic_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS Topics (
+        module_id  INTEGER NOT NULL,
+        topic_id   INTEGER NOT NULL,
         topic_name TEXT NOT NULL,
         topic_text TEXT,
         PRIMARY KEY(module_id, topic_id),
         FOREIGN KEY(module_id) REFERENCES Modules(module_id) ON DELETE CASCADE
 );
 
-CREATE TABLE AnsweredQuestions (
-        module_id INTEGER NOT NULL,
-        topic_id INTEGER NOT NULL,
-        question_id INTEGER NOT NULL,
-        question_text TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS Questions (
+        question_id          INTEGER PRIMARY KEY AUTOINCREMENT,
+        module_id            INTEGER NOT NULL,
+        topic_id             INTEGER NOT NULL,
+        user_name            TEXT,
+        question_text        TEXT NOT NULL,
         question_answer_text TEXT,
-        FOREIGN KEY(module_id, topic_id)
-                REFERENCES Topics(module_id, topic_id) ON DELETE CASCADE
+        FOREIGN KEY(module_id, topic_id)   REFERENCES Topics(module_id, topic_id)   ON DELETE CASCADE,
+        FOREIGN KEY(user_name)             REFERENCES Users(user_name)              ON DELETE CASCADE
 );
 
-CREATE TABLE OpenQuestions (
-        module_id INTEGER NOT NULL,
-        topic_id INTEGER NOT NULL,
-        question_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        question_text TEXT NOT NULL,
-        question_answer_text TEXT,
-        FOREIGN KEY(module_id, topic_id)
-                REFERENCES Topics(module_id, topic_id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS Users (
+        user_name TEXT NOT NULL,
+        role_id   INTEGER NOT NULL,
+        psswd     TEXT,
+        FOREIGN KEY(role_id) REFERENCES UserRoles(role_id) ON DELETE RESTRICT,
+        PRIMARY KEY(user_name, role_id)
 );
 
-CREATE VIEW TopicsView AS
-SELECT Modules.module_id AS module_id,
+CREATE TABLE IF NOT EXISTS UserRoles (
+        role_id      INTEGER PRIMARY KEY AUTOINCREMENT,
+        role_meaning TEXT NOT NULL
+);
+
+CREATE VIEW IF NOT EXISTS TopicsView AS
+SELECT Modules.module_id   AS module_id,
        Modules.module_name AS module_name,
-       Topics.topic_id AS topic_id,
-       Topics.topic_name AS topic_name,
-       Topics.topic_text AS topic_text
+       Topics.topic_id     AS topic_id,
+       Topics.topic_name   AS topic_name,
+       Topics.topic_text   AS topic_text
 FROM Topics
 LEFT JOIN Modules ON
 Topics.module_id = Modules.module_id;
